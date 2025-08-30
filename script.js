a82ef8ee92625f33081abaee107f2768e1d27828
rismayanfitri546-crypto <rismayanfitri546@gmail.com>
2025-08-30 15:58:32 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Script untuk demo interaktif tombol
+document.addEventListener("DOMContentLoaded", () => {
+  const btn = document.querySelector(".hero .btn");
+  btn.addEventListener("click", () => {
+    alert("Terima kasih sudah melihat produk Rakka Petshop!");
+  });
+});
