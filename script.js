6f9fe2ed463dd9d2d7b118a3b22480c320bd31d5
rismaaayannnn <rismayanfitri546@gmail.com>
2025-10-17 14:42:53 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Script untuk demo interaktif tombol
-document.addEventListener("DOMContentLoaded", () => {
-  const btn = document.querySelector(".hero .btn");
-  btn.addEventListener("click", () => {
-    alert("Terima kasih sudah melihat produk Rakka Petshop!");
-  });
-});
